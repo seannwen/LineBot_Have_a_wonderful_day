16f0ce9b199ab6d6c9f371d5fb27061e14778c25
ironsean <luckymanyo@gmail.com>
2021-04-24 02:28:29 +0800

    fix nba bug & recover orginal version

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-from var.variable import ACCESS_TOKEN, CHANNEL_SECRET
+# from var.variable import ACCESS_TOKEN, CHANNEL_SECRET
 import os
 import nba
 
@@ -16,12 +16,13 @@ app = Flask(__name__)
 # Channel Access Token
 access_token = os.environ.get('ACCESS_TOKEN')
 #access_token = 'ACCESS_TOKEN'
-line_bot_api = LineBotApi(access_token)
+line_bot_api = LineBotApi(ACCESS_TOKEN)
 
-# Channel Secretos.environ.get('ACCESS_TOKEN')
+# Channel Secret
 channel_secret = os.environ.get('CHANNEL_SECRET')
 #channel_secret = 'CHANNEL_SECRET'
-handler = WebhookHandler(channel_secret)
+#handler = WebhookHandler(channel_secret)
+handler = WebhookHandler(CHANNEL_SECRET)
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -41,12 +42,10 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #error_message = "請輸入想查詢的NBA隊名縮寫(Ex: LAL)"
-    #team_not_found_message = "查無此隊伍，請重新輸入。"
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
