22ea651c63514202bdbfb5927042ce25a3620267
ironsean <luckymanyo@gmail.com>
2021-04-26 20:44:00 +0800

    recover app.py

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    nba_response = nba #blablablabla
-    message = TextSendMessage(text=nba_response)
+    #nba_response = nba #blablablabla
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 
