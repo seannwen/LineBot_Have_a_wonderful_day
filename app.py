11a88b89db34bdd99cecbb815fc6d3a9d8ed12c6
ironsean <luckymanyo@gmail.com>
2021-05-11 12:40:46 +0800

    add nba function

--- app.py
+++ app.py
@@ -19,12 +19,10 @@ app = Flask(__name__)
 # Channel Access Token
 
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
-
 line_bot_api = LineBotApi(ACCESS_TOKEN)
 
 # Channel Secret
 CHANNEL_SECRET = os.environ.get('CHANNEL_SECRET')
-#handler = WebhookHandler(channel_secret)
 handler = WebhookHandler(CHANNEL_SECRET)
 
 # 監聽所有來自 /callback 的 Post Request
@@ -55,11 +53,12 @@ def handle_message(event):
 
     # NBA & PTT
     if split_message[0].lower() == 'nba':
-        nba_team_dict = nba.team_name()
-        nba_web = nba.get_web_data(date)  # get the daily data from website
-        nba_daily_score = nba.get_daily_score(nba_web)
+        # nba_team_dict = nba.team_name()
+        nba_web = nba.get_web_data()  # get the daily data from website
+        nba_daily_score = '\n'.join(nba.get_daily_score())
 
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=nba_daily_score))
+
 
     elif split_message[0].lower() == 'ptt':
             current_page = 'https://www.ptt.cc/bbs/{}/index.html'.format(split_message[1])
@@ -75,23 +74,6 @@ def handle_message(event):
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
-
-
-    # if len(split_message) == 2:
-    #     if split_message[0].lower() == 'ptt':
-    #         current_page = 'https://www.ptt.cc/bbs/{}/index.html'.format(split_message[1])
-    #         articles_info = []
-    #         bs4_html = ptt.get_bs4_html(current_page)
-    #         if not bs4_html.find('title') or bs4_html.find('title').text == '404':
-    #             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=page_not_found_message))
-    #         else:
-    #             articles_info = ptt.browse(articles_info, bs4_html)
-    #             articles = '\n'.join(articles_info)
-    #             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=articles))
-    #     else:
-    #         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=error_message))
-    # else:
-    #     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=error_message))
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
--- nba.py
+++ nba.py
@@ -1,15 +1,19 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import json
 import requests
 from bs4 import BeautifulSoup, element
 from datetime import datetime, timedelta
-#from nba_email import send_email
 from prettytable import PrettyTable
+from PIL import Image, ImageDraw, ImageFont
+from matplotlib import pyplot as plt
+from IPython.display import display
 
-
-def get_web_data(date):
+def get_web_data():
     # get the score data from the nba website
+
+    today = datetime.today()
+    date = today.strftime("%Y%m%d")
     res = requests.get('https://data.nba.net/prod/v2/{}/scoreboard.json'.format(date))
     json_object = json.loads(res.text)
     # print (json_object)
@@ -36,16 +40,18 @@ def insert_sting_middle(str, word):
     return str[:2] + word + str[2:]
 
 
-def get_daily_score(web):
+def get_daily_score():
     # get the score data from the nba website
+    web = get_web_data()
     message = []
+
     date = []
     today = datetime.today()
     date = today.strftime("%Y%m%d")
-    message.append('Date: {0}/{1}/{2}'.format(date[:4], date[4:6], date[6:]))
-    message.append('{0}Schedule{0}\n'.format('-' * 30))
+    #message.append('Date: {0}/{1}/{2}'.format(date[:4], date[4:6], date[6:]))
+    #message.append('{0}Schedule{0}\n'.format('-' * 30))
     team_dict = team_name()
-    
+
     # get the data of daily games
     for index, game in enumerate(web['games']):
         host = game['hTeam']
@@ -69,148 +75,47 @@ def get_daily_score(web):
             'N/A' if not host['score'] else host['score']
         ])
 
-        message.append('{0} Game {1:02} {0}\n'.format('=' * 21, index + 1))
-        message.append(table.get_string())
+
+        message.append(str('({0}) {1} (W{2}/L{3}) - {4}'.format(visitor['triCode'], team_dict[visitor['triCode']], visitor['win'], visitor['loss'],
+        'N/A' if not visitor['score'] else visotor['score'])))
+        message.append(str('({0}) {1} (W{2}/L{3}) - {4}'.format(host['triCode'], team_dict[host['triCode']], host['win'], host['loss'],
+        'N/A' if not host['score'] else host['score'])))
+
+
         message.append('Location: {}'.format(location['name']))
         message.append('Time: {}'.format(local_time.strftime('%Y/%m/%d %H:%M')))
-        message.append('Info: {}\n'.format('N/A' if not info['text'] else info['text']))
+        message.append('')
+        #message.append('Info: {}\n'.format('N/A' if not info['text'] else info['text']))
     return message
 
-
-def next_game(team):
-    # find the following game
-    message.append('{0}Preview{0}\n'.format('-' * 40))
-    i = 1
-    while True:
-        now = datetime.now()
-        # date = datetime.now()
-        aDay = timedelta(days=i)
-        now = now + aDay
-        date = now.strftime('%Y%m%d')
-
-        for game in get_web_data(date)['games']:
-            for x in game:
-                host = game['hTeam']
-                visitor = game['vTeam']
-                if host['triCode'] == '{}'.format(team) or visitor['triCode'] == '{}'.format(team):
-
-                    location = game['arena']
-                    team_A = team_dict[(host['triCode'])]
-                    team_B = team_dict[(visitor['triCode'])]
-
-                    start_time = game['startTimeUTC']
-                    utc_format = '%Y-%m-%dT%H:%M:%S.%fZ'
-                    utc_time = datetime.strptime(start_time, utc_format)
-                    local_time = utc_time + timedelta(hours=-8)
-                    if host['triCode'] == '{}'.format(team):
-                        series_win = host['seriesWin']
-                        series_loss = host['seriesLoss']
-                    else:
-                        series_win = visitor['seriesWin']
-                        series_loss = visitor['seriesLoss']
-
-                    message.append("The next game for the {}({}) will be on {}/{}/{}\n{} (W{}\
-/L{}) vs {} (W{}/L{})\nThe {}({}) in this series (W{}/L{})\nLocation: {}\nTime: {} \n"
-                        .format(team_dict[(team)],
-                                team,
-                                date[:4], date[4:6], date[6:],
-                                team_dict[(visitor['triCode'])],
-                                visitor['win'],
-                                visitor['loss'],
-                                team_dict[(host['triCode'])],
-                                host['win'],
-                                host['loss'],
-                                team_dict[(team)],
-                                team,
-                                series_win,
-                                series_loss,
-                                location['name'],
-                                local_time.strftime('%H:%M')))
-                    return team_A, team_B
-        i += 1
-
-
-def get_hist_score(team_A, team_B):
-
-    message.append('{0}History of Two Teams{0}\n'.format('-' * 30))
-    month = ['june', 'may', 'april', 'march', 'feburary', 'january', 'december', 'november', 'october']
-    year = ['2018', '2017']
-
-    win = 0
-    for i in range(len(year)):
-        for j in range(len(month)):
-            try:
-                res = requests.get('https://www.basketball-reference.com/leagues/NBA_{}_games-{}.html'.format(year[i], month[j]))
-                bs4_html = BeautifulSoup(res.text, "html.parser")
-                history_games = bs4_html.find_all('tr')
-
-                for game in history_games[1:]:
-                    # put data into dictioners of home and visitor
-                    home = {
-                        'date': game.find('th', {'data-stat': 'date_game'}).text,
-                        'name': game.find('td', {'data-stat': 'home_team_name'}).text,
-                        'points': game.find('td', {'data-stat': 'home_pts'}).text
-                    }
-
-                    visitor = {
-                        'name': game.find('td', {'data-stat': 'visitor_team_name'}).text,
-                        'points': game.find('td', {'data-stat': 'visitor_pts'}).text
-                    }
-
-                    if (
-                        (team_A == home['name'] and team_B == visitor['name']) or
-                        (team_B == home['name'] and team_A == visitor['name'])
-
-                    ):
-                        if home['points'] > visitor['points']:
-                            win += 1.0
-                        else:
-                            pass
-                        'N/A' if not home['points'] else visitor['points']
-
-                        message.append('{:>17} : {:>23} - {:>3}  vs  {:>23} - {:>3}'.format(
-                                home['date'],
-                                home['name'],
-                                home['points'],
-                                visitor['name'],
-                                visitor['points']))
-                        # percentage = (win / ) * 100
-
-            except AttributeError:
-                pass
-    # message.append('The winning percentage is {0}%'.format(percentage))
-
-
 if __name__ == "__main__":
-    message = []
+    # message = []
 
-    team_dict = team_name()
+    # team_dict = team_name()
 
     #date = input('Please enter the date(Ex: 20180101)(Default: Present): \n')
-    date = []
-    if not date:
-        datetime = datetime.today()
-        date = datetime.strftime("%Y%m%d")
-    else:
-        date
+    # date = []
+    # if not date:
+    #     datetime = datetime.today()
+    #     date = datetime.strftime("%Y%m%d")
+    # else:
+    #     date
 
-    # date = '20170306'
+    # date = '20210510'
 
     #team_abbr = input("Please enter a team name with abbreviation(Default: LAL): \n")
-    team_abbr = []
-    if not team_abbr:
-        team = 'LAL'
-    else:
-        team = team_abbr
+    # team_abbr = []
+    # if not team_abbr:
+    #     team = 'LAL'
+    # else:
+    #     team = team_abbr
     # team = insert_sting_middle('''''', team_abbr.upper())
 
-    web = get_web_data(date)  # get the data from website
-    get_daily_score(web)
-    # print '\n'.join(message)
+    # web = get_web_data()  # get the data from website
+    # get_daily_score(web)
+
     #next_game = next_game(team)  # get the data of next game
-    # get_hist_score(next_game[0], next_game[1])
 
-    #print ('\n'.join(message))
-
-    # email_subject = 'NBA daily report!!\n'
-    # send_email(email_subject, '\n'.join(message))
+    # print('\n'.join(get_daily_score()))
+    print(str(get_daily_score()))
+    # print ('\n'.join(message))
